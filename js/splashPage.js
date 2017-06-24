304278168faab7eacd5d903322c5bf6766c16b13
SKalt <kalt.steven@gmail.com>
2017-06-24 17:37:18 -0400

    temp rollup setup

--- js/splashPage.js
+++ js/splashPage.js
@@ -1,3 +1,4 @@
+import { geomToGml } from 'geojson-to-gml-3';
 /* toggles */
 $('.portfolio-toggle').on('click', function () {
     $(this).next('.portfolio-item').slideToggle(300);
@@ -5,181 +6,7 @@ $('.portfolio-toggle').on('click', function () {
 });
 
 /* Geojson -> GML example */
-//TODO: use webpack to manage imports instead of copying/pasting
 
-/* DISCLAIMER: gml 2.1.2-compliant only
-This hack only supports GML simple features.
-GML models from https://docs.oracle.com/cd/E11882_01/appdev.112/e11829/oracle/spatial/util/GML.html
- */
-/**
- * returns a string with the first letter capitalized.
- * @param {string} str
- * @returns {string} a string with the first letter capitalized.
- */
-function capitalizeFirstLetter(str) {
-    return str.replace(/^./, (letter) => letter.toUpperCase());
-}
-/**
- * returns a string with the first letter lowered.
- * @param {string} str
- * @returns {string} a string with the first letter lowered.
- */
-function lowerFirstLetter(str) {
-    return str.replace(/^./, (letter) => letter.toLowerCase());
-}
-/** @constant */
-var converter = {
-    /** 
-     * @method 
-     * converts a geojson geometry Point to gml
-     * @param {number[]} coords the coordinates member of the geometry
-     * @param {string|undefined} srsName a string specifying SRS
-     * @returns {string} a string of gml describing the input geometry
-     */
-    'Point': function (coords, srsName) {
-        return `<gml:Point${(srsName ? `
-        srsName = "${srsName}"
-        ` : '')}>` +
-        '<gml:coordinates cs="," ts=" " decimal=".">' +
-        coords.join() +
-            '</gml:coordinates>' +
-            '</gml:Point>';
-    },
-    /**
-     * @method 
-     * converts a geojson geometry LineString to gml
-     * @param {number[][]} coords the coordinates member of the geometry
-     * @param {string|undefined} srsName a string specifying SRS
-     * @returns {string} a string of gml describing the input geometry
-     */
-    'LineString': function (coords, srsName) {
-        return `<gml:LineString${(srsName ? `
-        srsName = "${srsName}"
-        `:'')}>` +
-        '<gml:coordinates cs="," ts=" " decimal=".">' +
-        coords.join(' ') +
-            '</gml:coordinates>' +
-            '</gml:LineString>';
-    },
-    /**
-     * @method 
-     * converts a geojson geometry ring in a polygon to gml
-     * @param {number[][]} coords the coordinates member of the geometry
-     * @param {string|undefined} srsName a string specifying SRS
-     * @returns {string} a string of gml describing the input geometry
-     */
-    'LinearRing': function (coords, srsName) {
-        return `<gml:LinearRing${(srsName ? `
-        srsName = "${srsName}"
-        `:'')}>` +
-        '<gml:coordinates cs="," ts=" " decimal=".">' +
-        coords.join(' ') +
-            '</gml:coordinates>' +
-            '</gml:LinearRing>';
-    },
-    /**
-     * @method 
-     * converts a geojson geometry Point to gml
-     * @param {number[][][]} coords the coordinates member of the geometry
-     * @param {string|undefined} srsName a string specifying SRS
-     * @returns {string} a string of gml describing the input geometry
-     */
-    'Polygon': function (coords, srsName) {
-        // geom.coordinates are arrays of LinearRings
-        let polygon = `<gml:Polygon${(srsName ? `
-        srsName = "${srsName}"
-        `:'')}>` +
-        '<gml:outerBoundaryIs>' +
-        this.LinearRing(coords[0]) +
-            '</gml:outerBoundaryIs>';
-        if (coords.length >= 2) {
-            for (let linearRing of coords.slice(1)) {
-                polygon += '<gml:innerBoundaryIs>' +
-                    this.LinearRing(linearRing) +
-                    '</gml:innerBoundaryIs>';
-            }
-        }
-        polygon += '</gml:Polygon>';
-        return polygon;
-    },
-    /**
-     * @method 
-     * Handles multigeometries
-     * @param {Object} geom a geojson geometry object
-     * @param {string} name the name of the multigeometry, e.g. 'MultiPolygon'
-     * @param {string|undefined} srsName a string specifying the SRS
-     * @param {string} memberPrefix the prefix of a gml member tag
-     * @returns {string} a string of gml describing the input multigeometry
-     * @throws {} TODO******
-     */
-    '_multi': function (geom, name, srsName, memberPrefix = '') {
-        let multi = `<gml:${name} ${(srsName ? `srsName = "${srsName}"` : '')}>`;
-        for (let member of geom) {
-            var _memberPrefix = '';
-            if (member.type) {
-                // geometryCollection: memberPrefix should be '',
-                memberPrefix = lowerFirstLetter(member.type);
-                member = member.coordinates;
-            }
-            if (!memberPrefix) {
-                throw 'un-typed member ' + JSON.stringify(member);
-            } else {
-                _memberPrefix = capitalizeFirstLetter(memberPrefix);
-            }
-            multi += `<gml:${memberPrefix}Member>` +
-                converter[_memberPrefix](member, srsName = '') +
-                `</gml:${memberPrefix}Member>`;
-        }
-        multi += `</gml:${name}>`;
-        return multi;
-    },
-    /**
-     * converts a geojson geometry MultiPoint to gml
-     * @param {number[][]} coords the coordinates member of the geometry
-     * @param {string|undefined} srsName a string specifying SRS
-     * @returns {string} a string of gml describing the input geometry
-     */
-    'MultiPoint': function (coords, srsName) {
-        return this._multi(coords, 'MultiPoint', srsName, 'point');
-    },
-    /**
-     * converts a geojson geometry MultiLineString to gml
-     * @param {number[][][]} coords the coordinates member of the geometry
-     * @param {string|undefined} srsName a string specifying SRS
-     * @returns {string} a string of gml describing the input geometry
-     */
-    'MultiLineString': function (coords, srsName) {
-        return this._multi(coords, 'MultiLineString', srsName, 'lineString');
-    },
-    /**
-     * converts a geojson geometry MultiPolygon to gml
-     * @param {number[][][][]} coords the coordinates member of the geometry
-     * @param {string|undefined} srsName a string specifying SRS
-     * @returns {string} a string of gml describing the input geometry
-     */
-    'MultiPolygon': function (coords, srsName) {
-        return this._multi(coords, 'MultiPolygon', srsName, 'polygon');
-    },
-    /**
-     * converts a geojson geometry GeometryCollection to gml MultiGeometry
-     * @param {Object[]} geoms an array of geojson geometry objects
-     * @param {string|undefined} srsName a string specifying SRS
-     * @returns {} a string of gml describing the input GeometryCollection
-     */
-    'GeometryCollection': function (geoms, srsName) {
-        return this._multi(geoms, 'MultiGeometry', srsName, 'geometry');
-    }
-};
-
-/**
- * Translate geojson to gml 2.1.2 for any geojson geometry type
- * @param {Object} geom a geojson geometry object
- * @param {string|undefined} srsName a string specifying SRS
- * @returns {string} a string of gml describing the input geometry
- */
-function geomToGml(geom, srsName = 'EPSG:4326') {
-    return converter[geom.type](geom.coordinates || geom.geometries, srsName);
-}
 $('#translate-geojson').on('click', function () {
     console.log('clicked');
 
@@ -218,6 +45,9 @@ function formatXml(xml) {
 
     return formatted;
 }
+//TODO: GeoJSON -> WFS-T
+
+
 
 /* redirect on landing */
 $(document).ready(()=>$(window.location.hash).show());
---
+++ rollup.config.js
@@ -0,0 +1,33 @@
+import babel from 'rollup-plugin-babel';
+import eslint from 'rollup-plugin-eslint';
+import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+import replace from 'rollup-plugin-replace';
+import uglify from 'rollup-plugin-uglify';
+
+export default {
+  entry: 'src/scripts/main.js',
+  dest: 'js/main.min.js',
+  format: 'iife',
+  sourceMap: 'inline',
+  plugins: [
+    resolve({
+      jsnext: true,
+      main: true,
+      browser: true,
+    }),
+    commonjs(),
+    eslint({
+      exclude: [
+        'src/styles/**',
+      ]
+    }),
+    babel({
+      exclude: 'node_modules/**',
+    }),
+    replace({
+      ENV: JSON.stringify(process.env.NODE_ENV || 'development'),
+    }),
+    (process.env.NODE_ENV === 'production' && uglify()),
+  ],
+};
