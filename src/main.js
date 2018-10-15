7e6fce15858f0ca0172ce9941d18392f3f8f47ea
Steven Kalt <kalt.steven@gmail.com>
2018-10-14 20:39:17 -0400

    updated layout, revised some text, fixed the js

--- src/main.js
+++ src/main.js
@@ -1,17 +1,20 @@
 function toggleSection(id){
-  const el = byId(id)
+  const el = document.getElementById(id)
   if (!el) return
   return el.open = !el.open
 }
-
+function closestHref(e) {
+  return e.href || closestHref(e.parentElement)
+}
 function handle(ev = {}) {
   const {type} = ev
   if (!type || type === 'hashchange') {
-    toggleSection(location.hash.slice(1));
+    toggleSection(location.hash.slice(1))
   } else if (type === 'click') {
-    ev.preventDefault();
-    let id = (ev.target.href || ev.target.parentElement.href).split('#')[1];
-    if (location.hash !== `#${id}`) return location.hash = id;
+    ev.preventDefault()
+    ev.stopPropagation()
+    let id = closestHref(ev.target).split('#')[1]
+    if (location.hash !== `#${id}`) return location.hash = id
     toggleSection(id)
   }
 }
