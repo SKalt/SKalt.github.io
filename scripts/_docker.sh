89b439a3cae25ea29306dab241be40493ba693cd
Steven Kalt <kalt.steven@gmail.com>
2019-08-10 12:40:52 -0400

    enabled jekyll livereload

--- scripts/_docker.sh
+++ scripts/_docker.sh
@@ -5,6 +5,7 @@ docker run --rm \
   --volume="$GIT_ROOT_DIR/vendor/bundle:/usr/local/bundle" \
   --volume="$GIT_ROOT_DIR/node_modules/:/srv/jekyll/node_modules" \
   -p 4000:4000 \
+  -p 35729:35729 \
   --name jekyll \
   -it jekyll/jekyll:$JEKYLL_VERSION \
   "$@"
@@ -19,6 +20,7 @@ docker run --rm \
 #  - caching node_modules
 # more documentation on mouting at https://docs.docker.com/storage/volumes/
 # -p 4000:4000 exposes port 4000 from within the container as port 4000 outside.
+# port 35729 is for the livereload.js websocket.
 # -it: two flags, -i -> 'interactive' + -t 'TTY' or 'terminal'
 # jekyll/jekyll:$JEKYLL_VERSION: the image name to create a container from
 # "$@" forwards commands passed to this script.
--- scripts/serve.sh
+++ scripts/serve.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-bash ./scripts/_docker.sh jekyll serve
+bash ./scripts/_docker.sh jekyll serve --livereload
