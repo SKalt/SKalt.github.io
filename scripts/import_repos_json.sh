8f9d6a7d1c925304e6b81d291ea5b7ffe140a635
Steven Kalt <kalt.steven@gmail.com>
2019-08-10 13:12:30 -0400

    added repo-download script

---
+++ scripts/import_repos_json.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+curl 'https://api.github.com/users/SKalt/repos' > ./_data/raw_repos.json;
+node -e "
+const yaml = require('js-yaml');
+console.log(
+  yaml.safeDump(
+    require('./_data/raw_repos')
+      .filter(repo => !repo.fork && !repo.private)
+      .sort((a, b) => new Date(b.created_at) - new Date(a.created_at))
+      .map(
+        repo => ({
+          identifier:  repo.name,
+          title:       repo.name,
+          description: repo.description,
+          link:        repo.homepage,
+          repo:        repo.html_url,
+          created_at:  repo.created_at.slice(0, 10),
+          updated_at:  repo.updated_at.slice(0, 10),
+          archived:    repo.archived
+        })
+      )
+    )
+)
+" > ./_data/slimmed_repos.yml
