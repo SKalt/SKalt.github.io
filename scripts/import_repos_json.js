78049150d46e8347b914ca703b8ecc5ea3bd8a3f
Steven Kalt <kalt.steven@gmail.com>
2020-02-15 16:57:36 -0500

    added cli for new posts/project listings

---
+++ scripts/import_repos_json.js
@@ -0,0 +1,49 @@
+#!/usr/bin/env node
+// const fs = require("fs");
+const https = require("https");
+const process = require("process");
+let searches = process.argv
+  .slice(process.argv.findIndex(arg => arg.includes("github.io")) + 1)
+  .map(s => new RegExp(s, "ig"));
+searches = searches.length ? searches : [/.*/gi];
+let url = "https://api.github.com/users/SKalt/repos";
+const get = url =>
+  new Promise((resolve, reject) => {
+    https.get(
+      {
+        hostname: "api.github.com",
+        path: "/users/SKalt/repos",
+        headers: { "User-Agent": "skalt" }
+      },
+      response => {
+        let buffer = "";
+        response.on("data", chunk => (buffer += chunk));
+        response.on("end", () => {
+          resolve(JSON.parse(buffer));
+        });
+      }
+    );
+  });
+
+get(url)
+  .then(json =>
+    json
+      .filter(
+        repo =>
+          !repo.fork &&
+          !repo.private &&
+          searches.some(search => search.test(repo.name))
+      )
+      .sort((a, b) => new Date(b.created_at) - new Date(a.created_at))
+      .map(repo => ({
+        identifier: repo.name,
+        title: repo.name,
+        description: repo.description,
+        link: repo.homepage,
+        repo: repo.html_url,
+        date: repo.created_at.slice(0, 10),
+        updated_at: repo.updated_at.slice(0, 10),
+        archived: repo.archived
+      }))
+  )
+  .then(console.log);
---
+++ scripts/new.sh
@@ -0,0 +1,63 @@
+#!/usr/bin/env bash
+here="${BASH_SOURCE[0]%/*}";
+
+function usage() {
+  echo "${BASH_SOURCE[0]%*/} [--post|--project]"
+}
+
+path_to_content="$(realpath "$here/../content")"
+path_to_posts="$path_to_content/_posts"
+path_to_projects="$path_to_content/_projects"
+
+function new-post() {
+  local title="$*"
+  file_path="$path_to_posts/$(date '+%F')-${title// /_}.md"
+  {
+    echo "---"
+    echo "title: $title"
+    echo "categories:"
+    echo "  - posts"
+    echo "---"
+  } > "$file_path"
+}
+
+
+function new-project() {
+  local title="$*"
+  local identifier
+  identifier="$(node -e "console.log('$title'.replace(/[-_ ]+/g, '_'))")"
+  local file_path="$path_to_projects/${identifier:-title_me}.md"
+  local dl
+  dl=$(mktemp)
+  node "$here/import_repos_json.js" "$title" > "$dl"
+  {
+    echo "---"
+    echo "identifier: $identifier"
+    echo "title: $title"
+    (grep -e 'link' "$dl" || echo "link:") | sed -e 's/^[[:space:]]*//'
+    (grep -e 'repo' "$dl" || echo "repo:") | sed -e 's/^[[:space:]]*//'
+    echo "---"
+  } > "$file_path"
+  echo "results: $dl"
+  cat "$dl"
+  echo "created $file_path"
+  # local file=""
+  # ---
+  # identifier: ascii-art
+  # title: "Converting images to ascii art in the browser"
+  # link: https://skalt.github.io/browser-ascii-art-converter
+  # repo: https://github.com/SKalt/browser-ascii-art-converter
+  # date: 2018-06-19
+  # ---
+}
+
+function main() {
+  local subcommand="$1"; shift;
+  case $subcommand in
+    --post|post)       new-post    "$@";;
+    --project|project) new-project "$@";;
+    *) usage && return 1;;
+  esac
+}
+
+main "$@"
